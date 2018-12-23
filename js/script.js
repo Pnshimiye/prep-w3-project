3934df8133a0a4cb2810d09705523a9ae2d32f42
Pnshimiye <pnshimiye@gmail.com>
2018-12-23 11:09:26 +0200

    add a condition for a fifth correct answer

--- js/script.js
+++ js/script.js
@@ -54,12 +54,12 @@ $(document).ready(function(){
       else{
         markFour=0;
       }
-      // if(answerQuestionFive === "right"){
-      //   markFive=2;
-      // }
-      // else{
-      //   markFive=0;
-      // }
+      if(answerQuestionFive === "right"){
+        markFive=2;
+      }
+      else{
+        markFive=0;
+      }
 
       // totalResult=markOne+markTwo+markThree+markFour+markFive
       
